874d93f60a844c532ddb9438e47fc6ee2f22c3d3
Grupposo Anthony <47425003+Nocktiss@users.noreply.github.com>
2019-05-27 18:24:15 +0200

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ const  mysql = require('mysql');
 const  connection = mysql.createConnection({
 host :  'localhost',
 user :  'root',
-password :  'FantasySarah147',
+password :  '****************',
 database :  'Wilders',
 });
 module.exports = connection;
